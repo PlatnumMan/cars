924b706e029e0844259c5c0914c223eab61ece3e
myles-ux <fistonrwanda@gmail.com>
2022-12-01 13:14:38 +0100

    commit

---
+++ main.js
@@ -0,0 +1,20 @@
+myCanvas.height = window.innerHeight;
+myCanvas.width = 200;
+
+const ctx = myCanvas.getContext("2d");
+const road = new Road(myCanvas.width / 2, myCanvas.width * 0.9);
+const car = new Car(100, 100, 30, 50);
+
+animate();
+
+function animate() {
+  car.update(road.borders);
+
+  myCanvas.height = window.innerHeight;
+
+  ctx.translate(0, -car.y + myCanvas.height * 0.7);
+  road.draw(ctx);
+  car.draw(ctx);
+
+  requestAnimationFrame(animate);
+}
